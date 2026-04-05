4102ded49de8773f34bd45d7106dd9f012fa1f02
souzaly <152198328+souzaly@users.noreply.github.com>
2026-04-05 08:40:59 -0300

    Change ownerId in config.js
    Updated ownerId in the configuration.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const { ActivityType } = require('discord.js');
 
 module.exports = {
-  ownerId: '1004206704994566164',
+  ownerId: '1141824276119429140',
   status: {
     rotateDefault: [
       { name: 'Netflix', type: ActivityType.Watching },
